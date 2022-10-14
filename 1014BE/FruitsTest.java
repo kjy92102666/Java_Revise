3c7486e1f737f8210f182f32852ad7be86844b12
kjy92102666 <76457749+kjy92102666@users.noreply.github.com>
2022-10-14 19:14:44 +0900

    Add files via upload

---
+++ 1014BE/FruitsTest.java
@@ -0,0 +1,38 @@
+package test;
+
+import javax.swing.JFrame;
+
+//사과장수.
+/*1.판매자는 과일 갯수, 판매 수익을 알고 있고 사과의 단가를 정할 수 있다.
+ * 2.판매자는 사과를 파는 행위를 할 수 있다.
+ * 3.판매자는 자신의 현재 잔액과 사과 갯수를 보여줄 수 있다.
+ * */
+
+//사과 구매자
+/*1.구매자는 자신이 보유한 사과 갯수와 금액을 알고 있다.
+ * 2.구매자는 사과를 사는 행위를 할 수 있다.
+ * 3.구매자는 현재 자신이 보유한 금액과 사과 갯수를 보여줄 수 있다.
+ * */
+public class FruitsTest extends JFrame{
+	
+	public FruitsTest() {
+		this.setTitle("과일판매기");
+		this.setSize(1000,1200);
+		
+		this.setLayout(null);
+		
+		
+		
+		this.setVisible(true);
+		this.setResizable(false);
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		boolean flat = false;
+		do {
+			
+		}while(flat=true);
+	}
+}
